e90b9eca3c04028b09abc067e7e3657f3897dfa4
lavish005 <lavishgargchd@gmail.com>
2024-08-24 13:22:19 +0530

    commit

---
+++ CLASS/LECTURES/LEC_22/data_type.js
@@ -0,0 +1,21 @@
+// There is no diiferent variable in js all are numbers 
+
+
+let a = 20;
+let b = 0.5;
+let c = -9;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+let d = "Hello";
+console.log(typeof(d));
+
+
+let j = null;
+console.log(typeof(null));
+
+
+let i = 9;
+i = i + 3;
+console.log(i);
---
+++ CLASS/LECTURES/LEC_22/variable.js
@@ -0,0 +1,22 @@
+// var  redeclare and assiginment possible
+
+var a = 10;
+console.log(a);
+var a = 20;
+console.log(a)
+
+// let    redeclare not possible and assignment possible
+
+let b = 10;
+console.log(b);
+// let b = 20;
+b = 20;
+console.log(b);
+
+// const        redclare and assignment not possible 
+
+const c = 30;
+console.log(c);
+// const c = 20;
+// c = 20;
+console.log(c);
